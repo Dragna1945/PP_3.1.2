8c56d93c0571c98cc641b35de77d041ff0726565
YaroslavShcherbakov <dragna1945@gmail.com>
2023-07-23 10:30:52 +0300

    работают не все страницы

---
+++ src/main/java/dao/UserDaoImp.java
@@ -0,0 +1,42 @@
+package dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import model.User;
+
+
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void addUser(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public User getUser(int id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    public void updateUser(int id, User user) {
+        user.setId(id);
+        entityManager.merge(user);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return entityManager.createQuery("from User", User.class).getResultList();
+    }
+
+    @Override
+    public void deleteUser(int id) {
+        entityManager.remove(entityManager.find(User.class, id));
+    }
+}
